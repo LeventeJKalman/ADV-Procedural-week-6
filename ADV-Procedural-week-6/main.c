c7d52d32f5ba151c38903add12abf192eb9964e0
EnhancedTarget <levente.kalman922@gmail.com>
2023-03-14 12:33:52 +0000

    up

--- ADV-Procedural-week-6/main.c
+++ ADV-Procedural-week-6/main.c
@@ -6,7 +6,7 @@ typedef struct node{
 	int num;
 
 	//reference to next node
-	struct node* next;
+	struct node* NEXT;
 }nodeT;
 
 void main() {
@@ -16,35 +16,40 @@ void main() {
 
 	//display a choice menu
 	printf("Press 1 to add to the start of the list\n");
-	printf("Press 2 to display the list\n");
+	printf("Press 2 to add to the end of the list\n");
+	printf("Press 3 to display the list\n");
 	printf("Press -1 to Exit\n");
 	scanf("%d", &menuOpt);
 
 	while (menuOpt != -1)
 	{
-		if (menuOpt == 1) {
-			printf("Add node to the start\n");
-
-			//creating and populating the node
-			temp = (nodeT*)malloc(sizeof(nodeT) * 1);
-			printf("please enter the number you would like to add:\n");
-			scanf("%d", &temp -> num);
-
-			//connecting the node to the list
-			temp->next = headerPtr;
+		if (menuOpt == 1)
+		{
+			printf("Add node at the start\n");
+			//Creating and populating the node
+			temp = (nodeT*)malloc(sizeof(nodeT*) * 1);
+			printf("Please enter the node number\n");
+			scanf("%d", &temp->num);
+			//Connecting the node to the list
+			temp->NEXT = headerPtr;
 			headerPtr = temp;
 		}
+		//adding to the end of the list
 		else if (menuOpt == 2) {
+			printf("");
+		}
+		else if (menuOpt == 3) {
 			temp = headerPtr;
 			while (temp != NULL)
 			{
 				printf("\nthe value of the node is: %d\n", temp -> num);
-				temp = temp->next;
+				temp = temp->NEXT;
 			}
 		}
 
 		printf("\nPress 1 to add to the start of the list\n");
-		printf("Press 2 to display the list\n");
+		printf("Press 2 to add to the end of the list\n");
+		printf("Press 3 to display the list\n");
 		printf("Press -1 to Exit\n");
 		scanf("%d", &menuOpt);
 	}
